95997e746c2e49f4eec21043f072d79ae859aa82
Marko Urbanc <urbancmarko1@gmail.com>
2024-08-10 23:07:19 +0200

    feat: :art: add colorful plots

---
+++ SpectralAnalysis/Code/different-windows.py
@@ -0,0 +1,64 @@
+"""
+Plot signal and spectrum with different window functions.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import scipy.signal as signal
+import scipy.fft as fft
+from analysis import *
+import cmasher as cmr
+
+# Constants
+SAMPLE_RATE = 44100
+
+# Use custom style
+mpl.style.use('./ma-style.mplstyle')
+
+# Load the signal
+data_path = './SuppliedData/val3.dat'
+data = np.loadtxt(data_path)
+data = data / np.max(data)
+data_len = len(data)
+data_time = 1/SAMPLE_RATE * data_len
+freq = np.linspace(0.0, 1.0/(2.0*(data_time/data_len)), data_len//2)
+bare_spectrum = fft.fft(data)
+bare_spectrum = bare_spectrum[:data_len//2]
+
+# Create windows
+windows = {
+    'Rectangular': signal.windows.boxcar(data_len),
+    'Blackman': signal.windows.blackman(data_len),
+    'Hann': signal.windows.hann(data_len),
+    'Bartlett': signal.windows.bartlett(data_len),
+    'Gaussian': signal.windows.gaussian(data_len, std=100),
+    'Hamming': signal.windows.hamming(data_len),
+    'Blackman': signal.windows.blackman(data_len),
+    'Kaiser': signal.windows.kaiser(data_len, beta=14)
+}
+
+# Create spectra
+spectra = {}
+for window in windows:
+    spectra[window] = fft.fft(data * windows[window])
+    spectra[window] = spectra[window][:data_len//2]
+
+# Plot the signal
+colors = ["#37123c","#d72483","#ddc4dd","#60afff","#98CE00"]
+cm = cmr.take_cmap_colors('cmr.tropical', len(windows), cmap_range=(0.0, 0.85))
+fig, ax = plt.subplots(1, 1, figsize=(10, 5), layout='compressed')
+
+# ax.plot(freq, abs(bare_spectrum), label='Bare', color=colors[0])
+for i, spectrum in enumerate(spectra):
+     ax.plot(freq, np.abs(np.abs(bare_spectrum) - np.abs(spectra[spectrum])), alpha=0.8,
+             color=cm[i], label=spectrum)
+
+ax.set_xlabel('Frequency (Hz)')
+ax.set_ylabel('Amplitude [arb. u.]')
+ax.legend(frameon=False)
+ax.set_title("Absolute Difference Between Bare and Windowed Spectra")
+ax.set_yscale('log')
+
+plt.suptitle(f"Signal {data_path.split('/')[-1]} Processed with Different Windows")
+plt.savefig(f"./SpectralAnalysis/Images/window-spectra-{data_path.split('/')[-1]}.png", dpi=500)
+plt.show()
---
+++ SpectralAnalysis/Code/peak-widths.py
@@ -0,0 +1,90 @@
+"""
+Calculate and fit the peak widths of the spectral lines using when
+using different windows.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import scipy.signal as signal
+import scipy.fft as fft
+from analysis import *
+import cmasher as cmr
+
+# Constants
+SAMPLE_RATE = 44100
+
+# Use custom style
+mpl.style.use('./ma-style.mplstyle')
+
+# Load the signal
+data_path = './SuppliedData/val2.dat'
+data = np.loadtxt(data_path)
+data = data / np.max(data)
+data_len = len(data)
+data_time = 1/SAMPLE_RATE * data_len
+freq = np.linspace(0.0, 1.0/(2.0*(data_time/data_len)), data_len//2)
+bare_spectrum = fft.fft(data)
+bare_spectrum = bare_spectrum[:data_len//2]
+
+# Create windows
+windows = {
+    'Rectangular': signal.windows.boxcar(data_len),
+    'Blackman': signal.windows.blackman(data_len),
+    'Hann': signal.windows.hann(data_len),
+    'Bartlett': signal.windows.bartlett(data_len),
+    'Gaussian': signal.windows.gaussian(data_len, std=100),
+    'Hamming': signal.windows.hamming(data_len),
+    'Blackman': signal.windows.blackman(data_len),
+    'Kaiser': signal.windows.kaiser(data_len, beta=14)
+}
+
+# Create spectra
+spectra = {}
+for window in windows:
+    spectra[window] = fft.fft(data * windows[window])
+    spectra[window] = spectra[window][:data_len//2]
+
+# Bare peak dimensions
+bare_peaks = signal.find_peaks(np.abs(bare_spectrum), height=50)
+bare_peak_widths = signal.peak_widths(np.abs(bare_spectrum), bare_peaks[0], rel_height=0.5)
+bare_peak_widths = bare_peak_widths[0] * SAMPLE_RATE / data_len
+
+# Calculate peak widths
+peaks = {}
+for window in windows:
+    peaks[window] = signal.find_peaks(np.abs(spectra[window]), height=30)
+    peaks[window] = np.array(peaks[window][1]["peak_heights"]) / bare_peaks[1]["peak_heights"]
+
+# Calculate peak widths -> FWHM
+peak_widths = {}
+for window in windows:
+    peak = signal.find_peaks(np.abs(spectra[window]), height=30)
+    peak_widths[window] = signal.peak_widths(np.abs(spectra[window]), peak[0], rel_height=0.5)
+    peak_widths[window] = (peak_widths[window][0] * SAMPLE_RATE/data_len) / bare_peak_widths[0]
+
+
+colors = ["#37123c","#d72483","#ddc4dd","#60afff","#98CE00"]
+cm = cmr.take_cmap_colors('cmr.tropical', len(windows), cmap_range=(0.0, 0.85))
+fig, ax = plt.subplots(1, 2, figsize=(12, 5), layout='compressed')
+x_axis = np.arange(len(windows))
+
+ax[0].set_title("Relative Peak Heights")
+ax[0].bar(x_axis, [np.mean(peaks[window]) for window in windows], color=cm, alpha=1,
+          zorder=10, edgecolor='black', linewidth=0.5)
+ax[0].set_xticks(x_axis)
+ax[0].set_xticklabels(windows.keys(), rotation=45)
+# ax[0].set_xlabel("Window Function")
+ax[0].set_ylabel("Peak Heights")
+
+
+ax[1].set_title("Relative Peak Widths")
+ax[1].bar(x_axis, [np.mean(peak_widths[window]) for window in windows], color=cm, alpha=1,
+          zorder=10, edgecolor='black', linewidth=0.5)
+ax[1].set_xticks(x_axis)
+ax[1].set_xticklabels(windows.keys(), rotation=45)
+# ax[1].set_xlabel("Window Function")
+ax[1].set_ylabel("Peak Widths")
+
+plt.suptitle(f"Relative Peak Heights and Widths for Different Windows on {data_path.split('/')[-1]}")
+plt.savefig(f"./SpectralAnalysis/Images/peak-widths-{data_path.split('/')[-1]}.png", dpi=500)
+plt.show()
