151878a9adc3773f99ee4b72711c3e47ef52334a
Marko Urbanc <urbancmarko1@gmail.com>
2024-08-11 15:23:09 +0200

    fix: :art: fix legacy colors

--- SpectralAnalysis/Code/signal-spectrum.py
+++ SpectralAnalysis/Code/signal-spectrum.py
@@ -8,6 +8,7 @@ import matplotlib as mpl
 import scipy.signal as signal
 import scipy.fft as fft
 from analysis import *
+import cmasher as cmr
 
 # Constants
 SAMPLE_RATE = 44100
@@ -16,7 +17,7 @@ SAMPLE_RATE = 44100
 mpl.style.use('./ma-style.mplstyle')
 
 # Load the signal
-data_path = './SuppliedData/val3.dat'
+data_path = './SuppliedData/val2.dat'
 data = np.loadtxt(data_path)
 data = data / np.max(data)
 data_len = len(data)
@@ -30,7 +31,8 @@ print(f"Peaks: {[freq[p] for p in peaks[0]]}")
 print(f"Peak Heights: {np.array(peaks[1]['peak_heights'])/np.max(np.abs(spectrum))}")
 
 # Load .wav 
-sound_data = load_wav('./GeneratedData/sig_4323_10463_12971_18245.wav')
+# sound_data = load_wav('./GeneratedData/sig_4323_10463_12971_18245.wav')
+sound_data = load_wav('./GeneratedData/sig_5880_9685.wav')
 print(sound_data["n"])
 sound = sound_data['signal'][:data_len] / np.max(sound_data['signal'])
 s_spectrum = fft.fft(sound)
@@ -38,26 +40,41 @@ s_spectrum = s_spectrum[:data_len//2]
 
 
 # Plot the signal
-colors = ["#37123c","#d72483","#ddc4dd","#60afff","#98CE00"]
+colors = cmr.take_cmap_colors('cmr.tropical', 8, cmap_range=(0.0, 0.85))
+
 fig, ax = plt.subplots(3, 1, figsize=(10, 8), layout='compressed')
 
-ax[0].plot(data, label='Signal', color=colors[0])
-ax[0].plot(sound, label='Recreation', color=colors[1], alpha=0.5)
+ax[0].plot(data, label='Signal', color=colors[2])
+ax[0].plot(sound, label='Recreation', color=colors[0], alpha=0.5)
 ax[0].set_xlabel('Sample')
 ax[0].set_ylabel('Amplitude [arb. u.]')
-ax[0].legend(frameon=True)
+ax[0].set_title(f"{data_path.split('/')[-1]} and its Recreation")
+l0 = ax[0].legend(frameon=True)
+l0.set_title('Signal')
+f0 = l0.get_frame()
+f0.set_edgecolor('black')
+f0.set_facecolor('white')
+f0.set_linewidth(0.5)
+f0.set_alpha(0.8)
 
-ax[1].plot(freq, np.abs(spectrum), label='Real', color=colors[3])
-ax[1].plot(freq, np.abs(s_spectrum), label='Recreation', color=colors[4])
+ax[1].plot(freq, np.abs(spectrum), label='Real', color=colors[2])
+ax[1].plot(freq, np.abs(s_spectrum), label='Recreation', color=colors[0])
 ax[1].set_xlabel('Frequency (Hz)')
 ax[1].set_ylabel('Amplitude [arb. u.]')
-ax[1].legend(frameon=False)
+ax[1].set_title('Spectrum of the Signals')
+l1 = ax[1].legend(frameon=True)
+l1.set_title('Signal')
+f1 = l1.get_frame()
+f1.set_edgecolor('black')
+f1.set_facecolor('white')
+f1.set_linewidth(0.5)
+f1.set_alpha(0.8)
 
-ax[2].plot(np.abs(data-sound), label='Difference', color=colors[1])
+ax[2].plot(np.abs(data-sound), label='Difference', color=colors[6])
 ax[2].set_xlabel('Sample')
 ax[2].set_ylabel('Absolute Difference')
-ax[2].legend(frameon=False)
 ax[2].set_yscale('log')
+ax[2].set_title('Absolute Difference between Signals')
 
 plt.suptitle("Input Signal Reconstruction")
 
--- SpectralAnalysis/Code/simple-plot.py
+++ SpectralAnalysis/Code/simple-plot.py
@@ -7,6 +7,7 @@ import matplotlib as mpl
 import scipy.signal as signal
 import scipy.fft as fft
 from analysis import *
+import cmasher as cmr
 
 # Constants
 SAMPLE_RATE = 44100
@@ -34,26 +35,26 @@ bare_spectrum2 = fft.fft(data2)
 bare_spectrum2 = bare_spectrum2[:data_len2//2]
 
 # Plot
-colors = ["#37123c","#d72483","#ddc4dd","#60afff","#98CE00"]
+colors = cmr.take_cmap_colors('cmr.tropical', 8, cmap_range=(0.0, 0.85))
 fig = plt.figure(figsize=(12, 8), layout='compressed')
 gs = fig.add_gridspec(2,2)
 ax = [fig.add_subplot(gs[0,0]), fig.add_subplot(gs[0,1]), fig.add_subplot(gs[1,:])]
 
 # Plot the 1. signal
-ax[0].plot(data, color=colors[1], alpha=0.8)
+ax[0].plot(data, color=colors[0], alpha=0.8)
 ax[0].set_xlabel('Sample')
 ax[0].set_ylabel('Amplitude [arb. u.]')
 ax[0].set_title(f"Raw Signal: {data_path.split('/')[-1]}")
 
 # Plot the 2. signal
-ax[1].plot(data2, color=colors[3], alpha=0.8)
+ax[1].plot(data2, color=colors[2], alpha=0.8)
 ax[1].set_xlabel('Sample')
 ax[1].set_ylabel('Amplitude [arb. u.]')
 ax[1].set_title(f"Raw Signal: {data_path2.split('/')[-1]}")
 
 # Plot both spectra
-ax[2].plot(freq, np.abs(bare_spectrum), label=f"{data_path.split('/')[-1]}", color=colors[1], alpha=0.8, lw=1.5)
-ax[2].plot(freq, np.abs(bare_spectrum2), label=f"{data_path2.split('/')[-1]}", color=colors[3], alpha=0.8, lw=1.5)
+ax[2].plot(freq, np.abs(bare_spectrum), label=f"{data_path.split('/')[-1]}", color=colors[0], alpha=0.8, lw=1.5)
+ax[2].plot(freq, np.abs(bare_spectrum2), label=f"{data_path2.split('/')[-1]}", color=colors[2], alpha=0.8, lw=1.5)
 ax[2].set_xlabel('Frequency (Hz)')
 ax[2].set_ylabel('Amplitude [arb. u.]')
 ax[2].set_title("Bare Spectra of Both Signals")
