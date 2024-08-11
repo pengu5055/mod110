1723749274dd2c8e8d4b9315281d3010f7f127a3
Marko Urbanc <urbancmarko1@gmail.com>
2024-08-11 18:50:15 +0200

    feat: :art: upload mega plot for signal1

--- WienerFilter/Code/reconstruct-1.py
+++ WienerFilter/Code/reconstruct-1.py
@@ -49,8 +49,28 @@ ax_u = [
     fig.add_subplot(gs_upper[0, 2]),
 ]
 # Remove the [0, 0] and [0, 2] axes
-ax_u[0].set_visible(False)
-ax_u[2].set_visible(False)
+ax_u[0].set_visible(True)
+# Use ax_u[0] for large plot title
+ax_u[0].text(0.5, 0.3, 'Reconstructed signal1.dat\nusing Wiener Filter', fontsize=16, ha='center',
+             fontfamily="IBM Plex Mono", fontweight='bold')
+ax_u[0].axis('off')
+ax_u[0].grid(False)
+ax_u[0].set_facecolor('none')
+ax_u[2].set_visible(True)
+# Create legend in ax_u[2]
+for i, (key, value) in enumerate(filtered.items()):
+    ax_u[2].plot([], [], color=colors[i], label=key)
+l = ax_u[2].legend(loc='center', ncols=2)
+l.set_title('Size of Wiener Filter Window')
+f = l.get_frame()
+f.set_facecolor('white')
+f.set_edgecolor('black')
+f.set_linewidth(0.5)
+f.set_alpha(0.8)
+ax_u[2].axis('off')
+ax_u[2].grid(False)
+ax_u[2].set_facecolor('none')
+
 ax = []
 for i in range(0, 8):
     for j in range(3):
@@ -58,29 +78,32 @@ for i in range(0, 8):
 
 for i, (key, value) in enumerate(filtered.items()):
     idx = 3*i
-    ax[idx].sharex(ax[0])
+    ax[idx].sharex(ax[21])
     ax[idx].plot(value, color=colors[i])
-    plt.setp(ax[idx].get_xticklabels(), visible=False)
-    plt.setp(ax[idx].get_xticklines() , visible=False)
+    ax[idx].set_ylim(0, 1)
+    ax[idx].set_yticks([0, 0.5, 1])
+    ax[idx].set_yticklabels([0, 0.5, " "], fontsize=10)
+    # plt.setp(ax[idx].get_xticklabels(), visible=False)
+    # plt.setp(ax[idx].get_xticklines() , visible=False)
 
 for i, (key, value) in enumerate(filtered.items()):
     idx = 3*i+1
-    ax[idx].sharex(ax[1])
+    ax[idx].sharex(ax[22])
     ax[idx].sharey(ax[idx-1])
     ax[idx].plot(np.abs(np.abs(signal0) - np.abs(value)), color=colors[i])
-    plt.setp(ax[idx].get_xticklabels(), visible=False)
-    plt.setp(ax[idx].get_xticklines() , visible=False)
+    # plt.setp(ax[idx].get_xticklabels(), visible=False)
+    # plt.setp(ax[idx].get_xticklines() , visible=False)
     plt.setp(ax[idx].get_yticklabels(), visible=False)
-    plt.setp(ax[idx].get_yticklines() , visible=False)
+    # plt.setp(ax[idx].get_yticklines() , visible=False)
 
 
 for i, (key, value) in enumerate(filtered.items()):
     idx = 3*i+2
-    ax[idx].sharex(ax[2])
+    ax[idx].sharex(ax[23])
     ax[idx].plot(freq, np.abs(fft.fft(value)[:len(value)//2]), color=colors[i])
     ax[idx].yaxis.tick_right()
-    plt.setp(ax[idx].get_xticklabels(), visible=False)
-    plt.setp(ax[idx].get_xticklines() , visible=False)
+    # plt.setp(ax[idx].get_xticklabels(), visible=False)
+    # plt.setp(ax[idx].get_xticklines() , visible=False)
 
 plt.setp(ax[21].get_xticklabels(), visible=True)
 plt.setp(ax[22].get_xticklabels(), visible=True)
@@ -91,10 +114,9 @@ plt.setp(ax[23].get_xticklines(), visible=True)
 ax[0].set_title('Wiener Filtered Signals')
 ax[1].set_title('Abs. Diff. b/w signal0.dat')
 ax[2].set_title('Spectrum')
-ax[21].set_xticklabels(np.arange(0, data_len, 100))
-ax[22].set_xticklabels(np.arange(0, data_len, 100))
+ax[21].set_xticks(np.arange(0, data_len, 100))
+ax[22].set_xticks(np.arange(0, data_len, 100))
 ax[23].set_xticks(np.arange(0, freq.max(), 5000))
-ax[23].set_xticklabels(np.arange(0, freq.max(), 5000))
 ax[23].set_xscale('log')
 ax[21].set_xlabel('Samples')
 ax[22].set_xlabel('Samples')
@@ -104,4 +126,5 @@ ax_u[1].plot(signal0, color="black")
 ax_u[1].set_title('Noiseless Signal')
 
 plt.subplots_adjust(top=0.95, bottom=0.05, left=0.05, right=0.95)
+plt.savefig("./WienerFilter/Images/reconstructed-signal1.png", dpi=700)
 plt.show()
